2c4ba0420a966295a1f9f67bc9a699eb81ae29e1
maarten <van_den_bergmaarten@hotmail.com>
2023-09-13 09:21:24 +0200

    amended code

--- contracts/LuksoCloneX.sol
+++ contracts/LuksoCloneX.sol
@@ -13,6 +13,7 @@ import {
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 bytes32 constant _LSP8_TOKEN_ID_TYPE = 0x715f248956de7ce65e94d9d836bfead479f7e70d69b718d47bfe7b00e05b4fe4;
+//this baseURI is the same for all tokens. It's a link to a folder on IPFS, where ipfs://urlToThatFolder/1 will point to the metadata of the first NFT metadata
 bytes32 constant _LSP8_TOKEN_METADATA_BASE_URI = 0x1a7628600c3bac7101f53697f48df381ddc36b9015e7d7c9c5633d1252aa2843;
 uint256 constant PUBLIC_PRICE_PER_TOKEN = 0.2 ether;
 uint256 constant MAX_SUPPLY = 100;
@@ -20,17 +21,19 @@ uint256 constant MAX_SUPPLY = 100;
 mapping(address => uint256) private _mintedTokensPerAddress;
 
 contract LuksoCloneX is LSP8IdentifiableDigitalAsset, ReentrancyGuard {
-    constructor(address owner_) LSP8IdentifiableDigitalAsset("LuksoCloneX", "lCloneX", owner_) {
+    //below LSP8IdentifiableDigitalAsset parent contract takes care of setting the name, symbol and owner. Here you would find the lines: super._setData(_LSP4_TOKEN_NAME_KEY, bytes(name_));  and:    super._setData(_LSP4_TOKEN_SYMBOL_KEY, bytes(symbol_));
+    constructor(address owner_) LSP8IdentifiableDigitalAsset("LuksoButtons", "BTNS", owner_) {
         // 4ed94534e6e56a4cf8cea54daa9fc9b59751668459433f8dba993763d5dc6e20 is the hash of the URI JSON content
         bytes memory jsonUrl = abi.encodePacked(
             bytes4(keccak256("keccak256(utf8)")),
             hex"4ed94534e6e56a4cf8cea54daa9fc9b59751668459433f8dba993763d5dc6e20",
-            bytes("ipfs://QmR6P52FwU8vVQRjqdi1AjHroVQRx35AzgFdTB3A9Jz5dx")
+            bytes("ipfs://QmR6P52FwU8vVQRjqdi1AjHroVQRx35AzgFdTB3A9Jz5dx") // link to metadata
         );
         // _setData is an internal method that sets data key/value pairs in a ERC725Y store: a generic data storage standard, used to provide LSPs with dynamic on-chain data storage space, that can be read, decoded and interpreted.
 
-        _setData(_LSP8_TOKEN_ID_TYPE, hex"02");
-        bytes memory zeroBytes = hex"00000000";
+        _setData(_LSP8_TOKEN_ID_TYPE, hex"02"); // 02 indicates string
+
+        bytes memory zeroBytes = hex"00000000"; // avoiding magic number
         bytes memory baseURI =
             abi.encodePacked(zeroBytes, bytes("ipfs://QmZh7P3YZNxFZUiHkXLNgAtdk2T6PAza3S15Jjg1DzxVGf"));
 
