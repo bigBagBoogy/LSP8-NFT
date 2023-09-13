15c84c51dc2ff861fa70dbdfe289f2cab0fecc3c
maarten <van_den_bergmaarten@hotmail.com>
2023-09-13 17:52:29 +0200

    amended code

--- contracts/LuksoCloneX.sol
+++ contracts/LuksoCloneX.sol
@@ -12,8 +12,9 @@ import {
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+//this _LSP8_TOKEN_ID_TYPE is the same for all NFT contracts using token id type uint2526.
 bytes32 constant _LSP8_TOKEN_ID_TYPE = 0x715f248956de7ce65e94d9d836bfead479f7e70d69b718d47bfe7b00e05b4fe4;
-//this baseURI is the same for all tokens. It's a link to a folder on IPFS, where ipfs://urlToThatFolder/1 will point to the metadata of the first NFT metadata
+//this _LSP8_TOKEN_METADATA_BASE_URI is the same for all NFT contracts. 
 bytes32 constant _LSP8_TOKEN_METADATA_BASE_URI = 0x1a7628600c3bac7101f53697f48df381ddc36b9015e7d7c9c5633d1252aa2843;
 uint256 constant PUBLIC_PRICE_PER_TOKEN = 0.2 ether;
 uint256 constant MAX_SUPPLY = 100;
@@ -31,26 +32,14 @@ contract LuksoCloneX is LSP8IdentifiableDigitalAsset, ReentrancyGuard {
         );
         // _setData is an internal method that sets data key/value pairs in a ERC725Y store: a generic data storage standard, used to provide LSPs with dynamic on-chain data storage space, that can be read, decoded and interpreted.
 
-        _setData(_LSP8_TOKEN_ID_TYPE, hex"02"); // 02 indicates string
+        _setData(_LSP8_TOKEN_ID_TYPE, hex"02"); // 02 indicates uint256
 
-        bytes memory zeroBytes = hex"00000000"; // here we are going to append the hex representation of 0 to the beginning of the bytes object of the ipfs URI
+        bytes memory zeroBytes = hex"00000000"; // here we are going to append the hex representation of 0 to the beginning of the bytes object of the ipfs TOKEN URI
         bytes memory baseURI =
-            abi.encodePacked(zeroBytes, bytes("ipfs://QmfBbkqTqzVtA7Mkya9bRmKkWLbYZQ1kyHRJBL1eiw8vgj"));
-
-        _setData(
-            0x4690256ef7e93288012f00000000000000000000000000000000000000000001,
-            abi.encodePacked(zeroBytes, bytes("ipfs://QmZh7P3YZNxFZUiHkXLNgAtdk2T6PAza3S15Jjg1DzxVGf/1"))
-        );
-        _setData(
-            0x4690256ef7e93288012f00000000000000000000000000000000000000000002,
-            abi.encodePacked(zeroBytes, bytes("ipfs://QmZh7P3YZNxFZUiHkXLNgAtdk2T6PAza3S15Jjg1DzxVGf/2"))
-        );
-        _setData(
-            0x4690256ef7e93288012f00000000000000000000000000000000000000000003,
-            abi.encodePacked(zeroBytes, bytes("ipfs://QmZh7P3YZNxFZUiHkXLNgAtdk2T6PAza3S15Jjg1DzxVGf/3"))
-        );
-
+            abi.encodePacked(zeroBytes, bytes("ipfs://QmfBbkqTqzVtA7Mkya9bRmKkWLbYZQ1kyHRJBL1eiw8vgj"));// folder on ipfs containing the metadata files of the nft's
+   
         _setData(_LSP8_TOKEN_METADATA_BASE_URI, baseURI);
+        
         // We set the length of the array to 1
         _setData(_LSP4_CREATORS_ARRAY_KEY, hex"01");
 
